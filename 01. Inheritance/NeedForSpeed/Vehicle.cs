ae5541d9148f9b2f90f5de6de75288aa64da7c4e
Ina Kavalova <135525228+Inatkata@users.noreply.github.com>
2024-02-29 07:52:40 +0100

    Add files via upload

---
+++ 01. Inheritance/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        public Vehicle(int horsePower, double fuel)
+        {
+            Fuel = fuel;
+            HorsePower = horsePower;
+        }
+        private const double DEFAULT_FUEL_CONSUMPTIION = 1.25;
+        public int HorsePower { get; set; }
+        public double Fuel { get; set; }
+
+        public virtual double FuelConsumption => DEFAULT_FUEL_CONSUMPTIION;
+
+        public virtual void Drive(double kilometers)
+        {
+            double fuelLeft = Fuel - FuelConsumption * kilometers;
+            if (fuelLeft >= 0)
+            {
+                Fuel = fuelLeft;
+            }
+        }
+    }
+}
