b39e4393eb05383d3276897cc346e20383cfb5be
Ina Kavalova <135525228+Inatkata@users.noreply.github.com>
2024-02-29 07:53:13 +0100

    Add files via upload

---
+++ 01. Inheritance/Restaurant/Cake.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const decimal CAKE_PRICE = 5;
+        private const double CAKE_GRAMS = 250;
+        private const double CAKE_CALORIES = 5;
+        public Cake(string name) : base(name, CAKE_PRICE, CAKE_GRAMS, CAKE_CALORIES)
+        {
+        }
+    }
+}
