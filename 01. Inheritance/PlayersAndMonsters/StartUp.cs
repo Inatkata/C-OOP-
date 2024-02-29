a3eb37b3d9e36b9ebe43bea8fd632b8f6e51c711
Ina Kavalova <135525228+Inatkata@users.noreply.github.com>
2024-02-29 07:52:09 +0100

    Add files via upload

---
+++ 01. Inheritance/PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var darkWizard = new DarkWizard("Dark", 4);
+            Console.WriteLine(darkWizard);
+        }
+    }
+}
